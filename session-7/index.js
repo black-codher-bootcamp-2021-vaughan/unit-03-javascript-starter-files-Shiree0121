5c52cb09332c8d130cc8d6d14bf4a4fb0c23f9e0
shiree0121 <shirewhite@hotmail.com>
2021-12-13 21:03:54 +0000

    task 1,2,3

--- session-7/index.js
+++ session-7/index.js
@@ -1,29 +1,42 @@
 // ****************************************************TASK 1****************************************************
 
+// const animals = ['cat' , 'dog' , 'wolf' , 'lion' , 'eagle' , 'zebra' ];
 
+// for (let index = 0; index < animals.length; index++) 
 
+// animals.pop();
+// animals.unshift( 'cow' );
+// animals.push( 'bird' );
 
-
-
-
-
-
-
-
-
-
+// console.log(animals );
 
 
 // ****************************************************TASK 2****************************************************
 
+//(task 2 part 1)
 
+const animals = ['cat' , 'dog' , 'wolf' , 'lion' , 'eagle' , 'zebra' ];
 
+animals.sort().reverse();
 
+console.log(animals );                // //has reversed the animal names 
 
+//(task 2 part 2)
 
+const names = ['bonny' , 'chan' , 'swv' , 'monika' , 'mel' , 'timmy' , 'tyrone' , 'zebrae' , 'samatha' , 'carrie' ];
 
+names.sort();
+names.reverse();
 
+console.log(names );
 
+//Extra Task
+
+const numbers = ['5' , '325' , '67' , '100000' , '150'];
+
+numbers.sort((a,b) => a - b);      //use that fomular then b-a for decending order 
+
+console.log(numbers);
 
 
 // ****************************************************TASK 3****************************************************
@@ -41,6 +54,8 @@ const people = [
 function compare(a, b) {
   const personA = a.age;
   const personB = b.age;
-
-  return personA > personB ? true : false;
+  console.log(a,b)
+  console.log(personA > personB ? true : false);
+  return 
 }
+console.log(people.sort((a,b) => compare(a,b)).map (item => item.name))
