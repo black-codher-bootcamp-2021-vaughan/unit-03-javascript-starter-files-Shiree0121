cd7704b0cb416af82bda7bf82b5bf0d8238e4f1f
shiree0121 <shirewhite@hotmail.com>
2021-12-15 20:34:47 +0000

    functions mousemove

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,54 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+//task 1
+
+let button=document.createElement('button')
+button.textContent='show population'
+button.onclick=displayPopulation
+document.body.appendChild(button)
+
+//createListOfFruits(fruits);
+
+//.filter((fruit) => fruit.length > 5)
+
+//mini task
+
+const x = document.querySelector("#clickMe");
+const y = document.querySelector("#hovering");
+
+x.addEventListener("click", respondClick);
+y.addEventListener("mouseover", respondMouseOver);
+y.addEventListener("mouseout", respondMouseOut);
+
+function respondMouseOver() {
+  document.querySelector("#effect").innerHTML += "MouseOver Event" + "<br>";
+}
+
+function respondMouseOut() {
+  document.querySelector("#effect").innerHTML += "MouseOut Event" + "<br>";
+}
+
+function respondClick() {
+  document.querySelector("#effect").innerHTML += "Click Event" + "<br>";
+}
+
+//Task 3 
+
+document
+  .querySelector("#remove-handler")
+  .addEventListener("mousemove", randomNumber)
+
+function randomNumber() {
+  document.querySelector("#random-number").innerHTML = Math.random()
+}
+
+document
+ .querySelector("#remove-handler-button")
+ .addEventListener("click", removeHandler);
+
+function removeHandler() {
+ document.querySelector("#remove-handler")
+ .removeEventListener("mousemove", randomNumber);
+} 
